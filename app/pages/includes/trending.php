94d6c8849ee5b6eb39f14c55da30e0265618ea85
Chhiring Lama <lamachhiring103@gmail.com>
2024-02-21 14:45:05 +0545

    Trending Section Update

---
+++ app/pages/includes/trending.php
@@ -0,0 +1,13 @@
+<div class="overflow-scroll-md">
+    <div class="trend-news-list">
+        <div class="trend-news">
+        <a href="<?= ROOT ?>/post/<?= $row['slug'] ?>">
+                <img src="<?= get_image($row['image']) ?>">
+                <h5><?= $row['title'] ?></h5>
+            </a>
+        </div>
+
+
+    </div>
+
+</div>
